5cdb2fc4f5454a7099214b0ac4cfc1937218ae05
Ben Hoppe <benhoppe@Endformats-MBP.fritz.box>
2021-11-07 14:58:07 +0100

    Add validator class

---
+++ src/blockchain/validator.ts
@@ -0,0 +1,15 @@
+export class Validators {
+  list
+  
+  constructor() {
+    this.list = [];
+  }
+
+  update(transaction) {
+    if (transaction.amount == 30 && transaction.to == "0") {
+      this.list.push(transaction.from);
+      return true;
+    }
+    return false;
+  }
+}
