b07a24d7bf0c58c7d1f6487f2e94bcc7305725ce
unknown <hoppe.ben97@gmail.com>
2022-03-08 01:15:59 +0100

    Create mocked block and mocked transactions

---
+++ tests/unit-tests/blockchain/mocked-block.ts
@@ -0,0 +1,54 @@
+import { TRANSACTION_TYPE } from "../../../src/config"
+import Block from "../../../src/blockchain/block"
+
+export const mockedBlock: Block = {
+  "timestamp": 1646668608462,
+  "lastHash": "genesis-hash",
+  "hash": "aa7faa7a7479a77429db8740595c0e60b6fdd8d69bd268809d708d0e1fe5cd19",
+  "data": [
+    {
+      "id": "31190ea0-9e2f-11ec-b5a1-b1aed34f9adc",
+      "type": TRANSACTION_TYPE.Transaction,
+      "input": {
+        "timestamp": 1646668606602,
+        "from": "502b5acaba0456d13955ca7b3da57455218ef2126282a631a885d7c5f77cbeaf",
+        "signature": "3AFD5C1A32B3444BAA9ED3B26E923C3F0359FB6FB6B8BA65183C4C683EF0E6057FE880105F8F6EB3ADE7883FD271399E5B33EEC7A1DE6C166DFC3D534A594707"
+      },
+      "output": {
+        "to": "b6902c1713cc9ef50316b3a9c3aba668581fee74fa4dbc82f3e8786e46855b11",
+        "amount": 149,
+        "fee": 1
+      }
+    },
+    {
+      "id": "319b36f0-9e2f-11ec-b5a1-b1aed34f9adc",
+      "type": TRANSACTION_TYPE.Transaction,
+      "input": {
+        "timestamp": 1646668607456,
+        "from": "502b5acaba0456d13955ca7b3da57455218ef2126282a631a885d7c5f77cbeaf",
+        "signature": "3AFD5C1A32B3444BAA9ED3B26E923C3F0359FB6FB6B8BA65183C4C683EF0E6057FE880105F8F6EB3ADE7883FD271399E5B33EEC7A1DE6C166DFC3D534A594707"
+      },
+      "output": {
+        "to": "b6902c1713cc9ef50316b3a9c3aba668581fee74fa4dbc82f3e8786e46855b11",
+        "amount": 149,
+        "fee": 1
+      }
+    },
+    {
+      "id": "321792e0-9e2f-11ec-b5a1-b1aed34f9adc",
+      "type": TRANSACTION_TYPE.Transaction,
+      "input": {
+        "timestamp": 1646668608270,
+        "from": "502b5acaba0456d13955ca7b3da57455218ef2126282a631a885d7c5f77cbeaf",
+        "signature": "3AFD5C1A32B3444BAA9ED3B26E923C3F0359FB6FB6B8BA65183C4C683EF0E6057FE880105F8F6EB3ADE7883FD271399E5B33EEC7A1DE6C166DFC3D534A594707"
+      },
+      "output": {
+        "to": "b6902c1713cc9ef50316b3a9c3aba668581fee74fa4dbc82f3e8786e46855b11",
+        "amount": 149,
+        "fee": 1
+      }
+    }
+  ],
+  "validator": "502b5acaba0456d13955ca7b3da57455218ef2126282a631a885d7c5f77cbeaf",
+  "signature": "88B71F6D15D0B7E6EF47B970755B50D485643D180680CD930B965EA98F505F44253A373FB7885F10801481C055CFCE5D519A258450A81E8C397445C76492240A"
+}
---
+++ tests/unit-tests/blockchain/mocked-transaction.ts
@@ -0,0 +1,61 @@
+import { TRANSACTION_TYPE } from "../../../src/config"
+
+export const mockedTransactionOne = {
+  "id": "62af2780-9e6e-11ec-89da-89e1f3a3718a",
+  "type": TRANSACTION_TYPE.Transaction,
+  "input": {
+    "timestamp": 1646695748088,
+    "from": "502b5acaba0456d13955ca7b3da57455218ef2126282a631a885d7c5f77cbeaf",
+    "signature": "3AFD5C1A32B3444BAA9ED3B26E923C3F0359FB6FB6B8BA65183C4C683EF0E6057FE880105F8F6EB3ADE7883FD271399E5B33EEC7A1DE6C166DFC3D534A594707"
+  },
+  "output": {
+    "to": "b6902c1713cc9ef50316b3a9c3aba668581fee74fa4dbc82f3e8786e46855b11",
+    "amount": 149,
+    "fee": 1
+  }
+}
+
+export const mockedTransactionTwo = {
+  "id": "62af2780-9e6e-11ec-89da-89e1f3a3718a",
+  "type": TRANSACTION_TYPE.Transaction,
+  "input": {
+    "timestamp": 1646695748088,
+    "from": "502b5acaba0456d13955ca7b3da57455218ef2126282a631a885d7c5f77cbeaf",
+    "signature": "3AFD5C1A32B3444BAA9ED3B26E923C3F0359FB6FB6B8BA65183C4C683EF0E6057FE880105F8F6EB3ADE7883FD271399E5B33EEC7A1DE6C166DFC3D534A594707"
+  },
+  "output": {
+    "to": "b6902c1713cc9ef50316b3a9c3aba668581fee74fa4dbc82f3e8786e46855b11",
+    "amount": 149,
+    "fee": 1
+  }
+}
+
+export const mockedTransactionValidatorFee = {
+  "id": "f348fa00-9e6e-11ec-8fae-9d04524d3462",
+  "type": TRANSACTION_TYPE.Validator_fee,
+  "input": {
+      "timestamp": 1646695990688,
+      "from": "537d981836c881477816ded2179bd9da1985e0fe27909f068d7baec3e45a838e",
+      "signature": "6B2D126EF4D3DDC48A0ED74E778237DB65FDF6A57CAE4137D21B2F0AA7FC091B3F74C09EE34F93C86BF873845A5129F9887338B1A9573C0F97EF7C682F09E70D"
+  },
+  "output": {
+      "to": "0",
+      "amount": 29,
+      "fee": 1
+  }
+}
+
+export const mockedTransactionStake = {
+  "id": "320af4a0-9e6f-11ec-8fae-9d04524d3462",
+  "type": TRANSACTION_TYPE.Stake,
+  "input": {
+      "timestamp": 1646696095978,
+      "from": "537d981836c881477816ded2179bd9da1985e0fe27909f068d7baec3e45a838e",
+      "signature": "6B2D126EF4D3DDC48A0ED74E778237DB65FDF6A57CAE4137D21B2F0AA7FC091B3F74C09EE34F93C86BF873845A5129F9887338B1A9573C0F97EF7C682F09E70D"
+  },
+  "output": {
+      "to": "0",
+      "amount": 29,
+      "fee": 1
+  }
+}
