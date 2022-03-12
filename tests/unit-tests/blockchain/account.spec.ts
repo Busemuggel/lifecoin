5a536991d64b35fc687fc43f607abf4bf084c420
unknown <hoppe.ben97@gmail.com>
2022-03-12 01:40:24 +0100

    Clean up tests

--- tests/unit-tests/blockchain/account.spec.ts
+++ tests/unit-tests/blockchain/account.spec.ts
@@ -3,6 +3,7 @@ import Account from '../../../src/blockchain/account'
 // import Transaction, * as nativeTransaction from '../../../src/wallet/transaction'
 // import * as nativeBlock from '../../../src/blockchain/block'
 import { INITAL_BALANCE, TRANSACTION_FEE } from '../../../src/config'
+import { TEST_ADDRESS, TEST_ADDRESS_BALANCE } from './mocked-address'
 
 /*
 jest.mock('../../../src/wallet/transaction')
@@ -15,10 +16,8 @@ const mockedBlock = nativeBlock as jest.Mocked<typeof nativeBlock>
 const Block = mockedBlock.default
 */
 
-describe('Environment', () => {
+describe('Account', () => {
   const account: Account = new Account()
-  const TEST_ADDRESS = "5985a172e5718fee6de7a6a22302394423578449052b280be2e64f2b545163e3"
-  const TEST_ADDRESS_BALANCE = 200
 
   const addressAndBalanceLoader = (): void => {
     account.addresses = [
@@ -40,7 +39,7 @@ describe('Environment', () => {
     // Block.mockClear()
   })
 
-  it('should add an adress to addresses', async () => {
+  it('should add an adress to account addresses', async () => {
     account.initialize(TEST_ADDRESS)
 
     expect(account.addresses).toStrictEqual(account.addresses = [
@@ -52,10 +51,13 @@ describe('Environment', () => {
   it('should transfer an 150 from an adress to another adress', async () => {
     account.transfer(account.addresses[0], TEST_ADDRESS, 150)
 
-    expect(account.addresses[1]).toStrictEqual(TEST_ADDRESS)
-    expect(account.balance["502b5acaba0456d13955ca7b3da57455218ef2126282a631a885d7c5f77cbeaf"])
-      .toBe(INITAL_BALANCE - 150)
-    expect(account.balance[TEST_ADDRESS]).toBe(150)
+    const result = account.addresses.find(e => e === TEST_ADDRESS)
+    const result2 = account.balance["502b5acaba0456d13955ca7b3da57455218ef2126282a631a885d7c5f77cbeaf"]
+    const result3 = account.balance[TEST_ADDRESS]
+
+    expect(result).toStrictEqual(TEST_ADDRESS)
+    expect(result2).toBe(INITAL_BALANCE - 150)
+    expect(result3).toBe(150)
   })
 
   it('should get the balance of the testAdress', async () => {
