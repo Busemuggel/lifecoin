1c297aaee73beac05bfeb8bdb1779256ebeb1520
unknown <hoppe.ben97@gmail.com>
2022-03-15 22:23:41 +0100

    Use jest.config.ts file instead of js

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  roots: ["./tests"],
+  testEnvironment: 'node',
+  setupFiles: ['dotenv/config'],
+  verbose: true,
+  testMatch: ["**/tests/**/*spec.ts"]
+}
+
+export default config;
