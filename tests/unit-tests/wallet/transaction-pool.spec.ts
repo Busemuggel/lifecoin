683ae7050bcc8a63c7ed5861707ff19fbb97c00d
unknown <hoppe.ben97@gmail.com>
2022-03-17 23:33:42 +0100

    Remove unnecessary spies

--- tests/unit-tests/wallet/transaction-pool.spec.ts
+++ tests/unit-tests/wallet/transaction-pool.spec.ts
@@ -10,40 +10,33 @@ describe('Transaction-Pool', () => {
   }
 
   beforeEach(() => {
-    jest.clearAllMocks()
     transactionPool.transactions = []
   })
 
   it('should check if the threshold is reached', async () => {
-    const spy = jest.spyOn(transactionPool, 'thresholdReached')
     transactionLoader()
 
     const result1 = transactionPool.thresholdReached()
     transactionPool.transactions.push(mockedTransactionStake)
     const result2 = transactionPool.thresholdReached()
 
-    expect(spy).toHaveBeenCalledTimes(2)
     expect(result1).toBe(false)
     expect(result2).toBe(true)
   })
 
   it('should add a transaction', async () => {
-    const spy = jest.spyOn(transactionPool, 'addTransaction')
     
     transactionPool.addTransaction(mockedTransactionOne)
 
-    expect(spy).toHaveBeenCalled()
     expect(transactionPool.transactions).toEqual([mockedTransactionOne])
   })
 
   it('should check if a transaction exists in the pool', async () => {
-    const spy = jest.spyOn(transactionPool, 'transactionExists')
     transactionLoader()
     
     const result1 = transactionPool.transactionExists(mockedTransactionOne)
     const result2 = transactionPool.transactionExists(mockedTransactionStake)
 
-    expect(spy).toHaveBeenCalledTimes(2)
     expect(transactionPool.transactions).toEqual(
       expect.arrayContaining([
         expect.objectContaining(result1)
@@ -52,12 +45,10 @@ describe('Transaction-Pool', () => {
   })
 
   it('should clear the transaction in the pool', async () => {
-    const spy = jest.spyOn(transactionPool, 'clear')
     transactionLoader()
 
     transactionPool.clear()
 
-    expect(spy).toHaveBeenCalled()
     expect(transactionPool.transactions).toEqual([])
   })
 })
